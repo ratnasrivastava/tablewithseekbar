f9ab95e41780fdc1740f551a672bc8cac666e5c7
Ratna Srivastava <er.ratnasrivastava@gmail.com>
2020-07-05 15:28:07 +0530

    MODIFIED :: called a separate funcction for tables

--- app/src/main/java/com/ratnasrivastava/tableswithseekbar/MainActivity.java
+++ app/src/main/java/com/ratnasrivastava/tableswithseekbar/MainActivity.java
@@ -1,40 +1,49 @@
 package com.ratnasrivastava.tableswithseekbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    public void generateTables(int number) {
+        final ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        for (int j = 1; j <= 10; j++) {
+            arrayList.add(j - 1, new Integer(j * number));
+        }
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+        listView = (ListView) findViewById(R.id.list_view);
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
         seekBar.setMax(10);
+        //where the seekbar cursor will be
+        seekBar.setProgress(1);
+        //from where cursor will be started, if don't set, it would be started from 0
         seekBar.setMin(1);
+        generateTables(1);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                ListView listView = (ListView) findViewById(R.id.list_view);
-                final ArrayList<Integer> arrayList = new ArrayList<Integer>();
-                //If you want to set at particular index, then first u need to initialize
-//                for(int j=0;j<10;j++){
-//                    arrayList.add(0);
-//                }
-                for(int j=1;j<=10;j++){
-                    arrayList.add(j-1, new Integer(j*i));
-                }
-                ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
-                listView.setAdapter(arrayAdapter);
+                generateTables(i);
+                /*
+               We can write content of generateTables here also , just we need to give context MainActivity.this
+
+                 */
+
             }
 
             @Override
